d79a841be342cc7252f813c1a66467b44a800334
KevinMartinezC <ceronk003@gmail.com>
2025-10-29 19:24:13 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by KevinMartinez on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
